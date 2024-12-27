1812b99d3d0d6559a14e56fc830c317788f10634
Guada Pascal <gp221@leicester.ac.uk>
2024-12-27 09:11:16 -0300

    Update ccda_app.py

--- ccda_app.py
+++ ccda_app.py
@@ -25,7 +25,10 @@ SPREADSHEET_ID = '1NtXDHphN_SC6fmAb2Ni6tYJGb7CiRgGuYqMJbclwAr0'
 
 # Configurar OpenAI
 try:
-    openai.api_key = st.secrets["openai_api_key"]
+    #openai.api_key = st.secrets["openai_api_key"]
+    client = OpenAI(
+        api_key=os.environ.get(st.secrets["openai_api_key"]),
+    )    
     st.write("Clave configurada correctamente.")
 except Exception as e:
     st.error(f"Error al configurar la clave: {e}")
