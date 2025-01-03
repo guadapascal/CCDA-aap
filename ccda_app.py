df622ce953134b4f68fa8506fed1744bf405f07e
Guada Pascal <gp221@leicester.ac.uk>
2025-01-03 13:26:03 -0300

    Update ccda_app.py

--- ccda_app.py
+++ ccda_app.py
@@ -273,7 +273,8 @@ if st.session_state["page_title"] or st.session_state["post_content"]:
             st.success("El contenido ha sido validado correctamente.")
 
 # ETAPA 2: Aplicar la evaluación automática de la contribución
-if st.session_state["post_content"] and st.session_state["evaluacion"] == "":
+if st.session_state["evaluacion_json"]:
+#if st.session_state["post_content"] and st.session_state["evaluacion"] == "":
     st.subheader("Ponderación por criterio de la contribución")
     st.session_state["evaluacion_json"] = evaluar_contribucion(st.session_state["post_content"])
     st.json(st.session_state["evaluacion_json"])
